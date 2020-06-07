f7c7f57f392c424cc33a2620c04d879609416f0a
ravinath <tudu.ravinath@gmail.com>
2020-06-07 20:11:45 +0530

    Updates

---
+++ precache-manifest.7cbbcd465ac2340e035b7ab54ff6dc64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7199dad0a3ee62766b08efe00cc7876",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "1b110439fef71324069d",
+    "url": "/first-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bb95520130bca59f8c9",
+    "url": "/first-react/static/js/2.5c3a8865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react/static/js/2.5c3a8865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b110439fef71324069d",
+    "url": "/first-react/static/js/main.02a2f4d6.chunk.js"
+  },
+  {
+    "revision": "ea205faa658c2bed7a00",
+    "url": "/first-react/static/js/runtime-main.cbcd1005.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
